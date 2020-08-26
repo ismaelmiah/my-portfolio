9ed1b530d89b5bb5c2574ec72798d6114a5e88d1
ismail5g <ismail27.dec@gmail.com>
2020-08-26 18:23:38 +0600

    Added react-mdl Material-Ui and Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="demo-big-content">
+      <Layout>
+        <Header title="Title" scroll>
+          <Navigation>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content"></div>
+        </Content>
+      </Layout>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 
 ReactDOM.render(
   <React.StrictMode>
