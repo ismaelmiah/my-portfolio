fe76bc1ea62a36c3696b958d18742fa322ba6814
Ismail Hossen <ismail96dream@gmail.com>
2021-03-10 23:52:30 +0600

    Update Profile

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -21,7 +21,7 @@ class Resume extends Component {
             <h2 style={{ paddingTop: "1em" }}>Ismael Miah</h2>
             <h4 style={{ color: "grey" }}>Asp .Net Developer</h4>
             <hr style={{ borderTop: "2px solid #833fb2", width: "50%" }} />
-            <p>
+            <p style={{ textAlign: "justify" }}>
               This is Ismail Hosen. Now i'm a final year student of BSC in
               Software Engineering Department at Daffodil International
               University. I liked to solve a new real world problem by coding.
@@ -47,7 +47,7 @@ class Resume extends Component {
               target="_blank"
               rel="noopener noreferrer"
               style={{ textDecoration: "none" }}
-              href="https://drive.google.com/file/d/1a8pQ1Fg6Dqxgc9knrPogmM0GFtOicKjK/view?usp=sharing"
+              href="https://drive.google.com/file/d/1Cpf_RC5pRDkje-dyRnmhld3VNP4xJ0hR/view?usp=sharing"
             >
               My Update CV
             </a>
@@ -62,6 +62,14 @@ class Resume extends Component {
             />
             <hr style={{ borderTop: "3px solid #e22947" }} />
             <h2>Experience</h2>
+            <Experience
+              startYear={"December 2020"}
+              endYear={"January 2020"}
+              jobName={"Internee"}
+              jobDesc={
+                "That was a part-time Internee. And I learnt lot of tech things from this company I now had a chance to test my knowledge in the real world through an internship."
+              }
+            />
             <Experience
               startYear={"Spring 2019"}
               endYear={"Fall 2019"}
@@ -79,11 +87,12 @@ class Resume extends Component {
 
             <hr style={{ borderTop: "3px solid #e22947" }} />
             <h2>Skills</h2>
-            <Skills skill={"C#"} progress={70} />
             <Skills skill={"HTML/CSS"} progress={80} />
-            <Skills skill={"ReactJS"} progress={50} />
-            <Skills skill={"Asp .Net Core"} progress={70} />
-            <Skills skill={"SQL Server"} progress={70} />
+            <Skills skill={"ReactJS"} progress={60} />
+            <Skills skill={"Angular"} progress={70} />
+            <Skills skill={"C#"} progress={90} />
+            <Skills skill={"Asp .Net Core"} progress={90} />
+            <Skills skill={"SQL Server"} progress={80} />
             <hr style={{ borderTop: "3px solid #e22947" }} />
             <h2>Achievments</h2>
             <h6>6th at SWE Junior Programming Contest (03/2019)</h6>
